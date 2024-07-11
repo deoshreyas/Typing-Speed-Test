afd7c162a97ca0bc988e2ca5b07fa212f60fef42
Shreyas Deo <shreyasdeo.k50@gmail.com>
2024-07-12 00:40:33 +0530

    Create blinking cursor to show highlighted character, Experiment with scrolling text as user types

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ function initTest() {
             to_add += "<span>" + word[j] + "</span>";
             chars.push(word[j]);
         }
+        to_add += "<span> </span>";
+        chars.push(" ");
     }
     document.getElementById("text").innerHTML = to_add;
     return chars;
@@ -26,18 +28,43 @@ function initTest() {
 
 var characters = initTest();
 
+var num_chars_typed = 0;
+
 document.getElementById("hidden-type").addEventListener("input", function() {
     var text = this.value.split("");
     var spans = document.getElementById("text").getElementsByTagName("span");
     for (var i = 0; i < characters.length; i++) {
         if (i<text.length) {
             if (text[i] == characters[i]) {
-                spans[i].style.color = "green";
+                spans[i].style.color = "var(--typed-text)";
             } else {
-                spans[i].style.color = "red";
+                spans[i].style.color = "var(--wrong)";
+            }
+            if (i+1==text.length) {
+                spans[i].classList.add("cursor");
+            } else {
+                spans[i].classList.remove("cursor");
             }
         } else {
-            spans[i].style.color = "black";
+            spans[i].style.color = "var(--untyped-text)";
+            if (spans[i].classList.contains("cursor")) {
+                spans[i].classList.remove("cursor");
+            }
         }
     }
+    num_chars_typed = text.length;
+    scrollLeft(num_chars_typed);
 });
+
+document.addEventListener("keydown", function(e) {
+    document.getElementById("hidden-type").focus();
+});
+
+function scrollLeft(n) {
+    var text_element = document.getElementById("text");
+    if (n < 10) {
+        text_element.style.left = "0";
+    } else {
+        text_element.style.left = -(n-10) + "ch";
+    }
+}
